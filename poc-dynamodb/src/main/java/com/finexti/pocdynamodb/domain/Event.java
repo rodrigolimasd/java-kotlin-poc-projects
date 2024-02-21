f81ac484f3629a3b53545c254e58e475acbee34b
Rodrigo Lima <rodrigolimasd@gmail.com>
2024-02-21 20:01:47 -0300

    feat: update event to use sortkey

--- poc-dynamodb/src/main/java/com/finexti/pocdynamodb/domain/Event.java
+++ poc-dynamodb/src/main/java/com/finexti/pocdynamodb/domain/Event.java
@@ -1,20 +1,25 @@
 package com.finexti.pocdynamodb.domain;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
 import com.finexti.pocdynamodb.util.LocalDateConverter;
 import com.finexti.pocdynamodb.util.LocalDateTimeConverter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamoDBTable(tableName = "event")
 public class Event {
+
     @DynamoDBHashKey(attributeName = "id")
-    @DynamoDBAttribute(attributeName = "date_event_id")
-    private String dateEventId; // example '2021-05-21#GUID'
+    private String id;
+
+    @DynamoDBRangeKey(attributeName = "date_event_id")
+    private String dateEventId;
 
     @DynamoDBAttribute(attributeName = "create_date")
     @DynamoDBTypeConverted(converter = LocalDateTimeConverter.class)
@@ -35,73 +40,4 @@ public class Event {
 
     @DynamoDBAttribute(attributeName = "note")
     private String note;
-
-    public Event() {
-    }
-
-    public Event(String dateEventId, LocalDateTime createDate, LocalDate date, double balance, double expense, double income, String note) {
-        this.dateEventId = dateEventId;
-        this.createDate = createDate;
-        this.date = date;
-        this.balance = balance;
-        this.expense = expense;
-        this.income = income;
-        this.note = note;
-    }
-
-    public String getDateEventId() {
-        return dateEventId;
-    }
-
-    public void setDateEventId(String dateEventId) {
-        this.dateEventId = dateEventId;
-    }
-
-    public LocalDateTime getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDateTime createDate) {
-        this.createDate = createDate;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public double getExpense() {
-        return expense;
-    }
-
-    public void setExpense(double expense) {
-        this.expense = expense;
-    }
-
-    public double getIncome() {
-        return income;
-    }
-
-    public void setIncome(double income) {
-        this.income = income;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
 }
--- poc-dynamodb/src/main/java/com/finexti/pocdynamodb/service/EventService.java
+++ poc-dynamodb/src/main/java/com/finexti/pocdynamodb/service/EventService.java
@@ -4,12 +4,14 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.finexti.pocdynamodb.domain.Event;
 import com.finexti.pocdynamodb.dto.EventDto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Log4j2
 @Service
 public class EventService {
     private final DynamoDBMapper dynamoDBMapper;
@@ -19,24 +21,26 @@ public class EventService {
     }
 
     public EventDto createNewEvent(EventDto eventDto) {
-        Event event = new Event();
-        event.setDate(eventDto.getDate());
-        event.setExpense(eventDto.getExpense());
-        event.setIncome(eventDto.getIncome());
-        event.setNote(eventDto.getNote());
-
-        String dateEventId = eventDto.getDate().toString() + "#" + UUID.randomUUID().toString();
-        event.setDateEventId(dateEventId);
-
-        event.setCreateDate(LocalDateTime.now());
-
+        Event event = Event.builder()
+                .id(eventDto.id())
+                .date(eventDto.date())
+                .expense(eventDto.expense())
+                .income(eventDto.income())
+                .note(eventDto.note())
+                .dateEventId("%s#%s".formatted(eventDto.date().toString(), UUID.randomUUID().toString()))
+                .createDate(LocalDateTime.now())
+                .build();
+        log.info("creating event: {}", event);
         dynamoDBMapper.save(event);
+        log.info("event created: {}", event);
         return eventDto;
     }
 
     public List<Event> getAllEvents() {
         DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+        log.info("scan expression: {}", scanExpression);
         List<Event> events = dynamoDBMapper.scan(Event.class, scanExpression);
+        log.info("events scanned: total events: {}", events.size());
         return events;
     }
 }
