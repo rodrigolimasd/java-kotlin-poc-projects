a526f0073d86e678b706f5aad782db8debd5eaa7
Marcio Rodrigo <rodrigobraga0125@gmail.com>
2024-02-21 17:24:39 -0300

    feat: criado o Event service e controller

---
+++ poc-dynamodb/src/main/java/com/finexti/pocdynamodb/controller/EventController.java
@@ -0,0 +1,29 @@
+package com.finexti.pocdynamodb.controller;
+
+import com.finexti.pocdynamodb.domain.Event;
+import com.finexti.pocdynamodb.dto.EventDto;
+import com.finexti.pocdynamodb.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+    @PostMapping
+    public EventDto createEvent(@RequestBody EventDto eventDto) {
+        return eventService.createNewEvent(eventDto);
+    }
+}
